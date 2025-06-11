f8ef92e16a6ef6c2ef70cda3b727153e532e58e1
WaffleToast <148481626+WaffleToast1814@users.noreply.github.com>
2025-06-11 12:44:40 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let toggleBtn = document.getElementById('bar')
 let slides = document.getElementsByClassName("MySlides")
 let slideIndex = 0
-let PlayableCharacterButton = document.querySelector('.Playable-Character-Button')
-let UnplayableCharacterButton = document.querySelector('.Unplayable-Character-Button')
 
 function toggle(){
     let links = document.querySelector('.mobile-menu')
@@ -32,20 +30,28 @@ function showSlides(){
 
 showSlides()
 
-function CharacterSwitch(){
-    let buttonContainer = document.querySelector('.buttonContainer')
-    let PlayableCharacterImage = document.querySelector('.Character-List-Image')
-    if (PlayableCharacterImage.style.display === "none") {
-        PlayableCharacterImage.style.display === "block";
-      } else {
-        PlayableCharacterImage.style.display === "none";
-      }
 
-    let UnPlayableCharacterImagev = document.querySelector('.Unplayable-Character-List-Image')
-}
 
 
 
 toggleBtn.addEventListener('click', toggle)
-PlayableCharacterButton.addEventListener('click' , CharacterSwitch)
-UnplayableCharacterButton.addEventListener('click' , CharacterSwitch)
+document.addEventListener('DOMContentLoaded', () => {
+  const playableBtn = document.querySelector('.Playable-Character-Button');
+  const unplayableBtn = document.querySelector('.Unplayable-Character-Button');
+  const playableList = document.querySelector('.Playable-Character-List-Image');
+  const unplayableList = document.querySelector('.Unplayable-Character-List-Image');
+
+  playableBtn.addEventListener('click', () => {
+    playableList.style.display = 'grid';
+    unplayableList.style.display = 'none';
+    playableList.classList.add('Playable-Character-List-Image')
+    playableList.style.gridTemplateColumns = 'repeat 1,2fr'
+  });
+
+  unplayableBtn.addEventListener('click', () => {
+    playableList.style.display = 'none';
+    unplayableList.style.display = 'grid';
+    unplayableList.classList.add('Unplayable-Character-List-Image')
+    unplayableList.style.gridTemplateColumns = 'repeat 1,2fr'
+  });
+});
